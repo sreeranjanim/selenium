7a2bec0b2fed375f5cfc403d0f247e99705de6d1
sreeranjanim <ranjani76674@gmail.com>
2022-12-29 17:42:33 +0530

    invoking the chrome browser

---
+++ introduction/src/introduction/seleniumintroduction.java
@@ -0,0 +1,17 @@
+package introduction;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class seleniumintroduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ranjani\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+
+	}
+
+}
