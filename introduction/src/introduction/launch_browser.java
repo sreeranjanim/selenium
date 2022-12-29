5ae5e6958ea1cc09ed4057ff9d8505d02eca5701
sreeranjanim <ranjani76674@gmail.com>
2022-12-29 18:48:24 +0530

    launching the browser website

---
+++ introduction/src/introduction/launch_browser.java
@@ -0,0 +1,16 @@
+package introduction;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class launch_browser {
+
+	public static void main(String[] args) {
+		 
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ranjani\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");;
+
+	}
+
+}
